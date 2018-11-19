b8b631bb88b82f79104edbe634c163525e7319a6
aznafro <joekwon123@yahoo.com>
2018-11-19 16:20:39 +0800

    add home page

--- src/js/sections/home.js
+++ src/js/sections/home.js
@@ -12,23 +12,25 @@ const home = () => {
 	homeContainer.classList.add("home__section");
 
 	// add header
+	headerDiv.classList.add("home__header--box");
 	homeHeader.classList.add("home__header");
 
 	headerSpan.classList.add("text");
 	headerSpan.textContent = "Nook Worm";
 
-	headerDiv.appendChild(homeHeader);
 	homeHeader.appendChild(headerSpan);
+	headerDiv.appendChild(homeHeader);
 	homeContainer.appendChild(headerDiv);
 
 	// add quote
+	quoteDiv.classList.add("home__quote--box");
 	homeQuote.classList.add("home__quote");
 
 	quoteSpan.classList.add("text");
 	quoteSpan.textContent = "\"Wholesome chit-chat with some noms to boot\"";
 
-	quoteDiv.appendChild(homeQuote);
 	homeQuote.appendChild(quoteSpan);
+	quoteDiv.appendChild(homeQuote);
 	homeContainer.appendChild(quoteDiv);
 
 	return homeContainer;
